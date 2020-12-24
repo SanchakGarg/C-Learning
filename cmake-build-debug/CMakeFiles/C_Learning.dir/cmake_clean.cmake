11259b521e73fc32a1f8a1814ae27b413a5ae93d
LiveHacker <48121742+sanchakgarg@users.noreply.github.com>
2020-12-24 12:14:40 +0530

    New Software
    Project can directly run on visual studio ,  vs-code and clion

---
+++ cmake-build-debug/CMakeFiles/C_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Learning.dir/main.cpp.obj"
+  "C_Learning.exe"
+  "C_Learning.exe.manifest"
+  "C_Learning.lib"
+  "C_Learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
